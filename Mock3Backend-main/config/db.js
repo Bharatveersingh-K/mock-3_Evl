242e5ef540200ea6bd84e1265d69b54729d44bb5
Bharatveer Singh <128880577+Bharatveersingh-K@users.noreply.github.com>
2023-09-11 23:25:03 +0530

    Delete Mock3Backend-main directory

--- Mock3Backend-main/config/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-require("dotenv").config()
-
-
-const connection = mongoose.connect("mongodb://localhost:27017/olx_classifieds_olx");
-module.exports = { connection };
